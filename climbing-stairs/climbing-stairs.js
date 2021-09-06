9d8aa9874639afa6080f7bfa4422d94aeced8585
Alex Tarnawski <69013606+out-spaced@users.noreply.github.com>
2021-09-06 11:59:00 -0700

    Time: 101 ms (13.24%), Space: 38.6 MB (49.56%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n, memo = {}) {
+    if (n in memo) {
+        return memo[n];
+    }
+    if (n === 0) {
+        return 1;
+    } else if (n < 0) {
+        return 0;
+    }
+    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+    return memo[n];
+};
