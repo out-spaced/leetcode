1ad0081823faa7d1839facb64b8ef4103ed29285
Alex Tarnawski <69013606+out-spaced@users.noreply.github.com>
2021-09-06 13:10:45 -0700

    Time: 168 ms (5.10%), Space: 40.1 MB (95.44%) - LeetHub

---
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    let current = head;
+    while (current !== null) {
+        while (current.next !== null && current.val === current.next.val) {
+            current.next = current.next.next;
+        }
+        current = current.next;
+    }
+    return head;
+};
