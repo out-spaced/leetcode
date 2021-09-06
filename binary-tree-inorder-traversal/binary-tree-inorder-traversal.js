0eb07217d22dff4b1fbd15ce51a02d0b25089b77
Alex Tarnawski <69013606+out-spaced@users.noreply.github.com>
2021-09-06 14:41:29 -0700

    Time: 72 ms (74.89%), Space: 39.9 MB (7.21%) - LeetHub

---
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if (root === null) {
+        return [];
+    }
+    let left = inorderTraversal(root.left);
+    let right = inorderTraversal(root.right);
+    left.push(root.val);
+    return left.concat(right);
+};
