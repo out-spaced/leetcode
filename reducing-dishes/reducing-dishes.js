465f10ca2cecd29991dab5695ae05f272b9786a6
Alex Tarnawski <69013606+out-spaced@users.noreply.github.com>
2021-09-20 20:54:25 -0700

    Time: 72 ms (92.86%), Space: 38.8 MB (75.00%) - LeetHub

--- reducing-dishes/reducing-dishes.js
+++ reducing-dishes/reducing-dishes.js
@@ -11,30 +11,17 @@ var maxSatisfaction = function(satisfaction) {
         }
         return 0;
     });
-    let firstPos = null;
-    let j = 0; 
+    
+    let j = satisfaction.length - 1; 
     let total = 0;
     let addedEachTime = 0;
-    let multiplier = 1;
-    while (j < satisfaction.length) {
-        if (satisfaction[j] >= 0) {
-            firstPos = j;
-            break;
-        }
-        j++;
+    while (satisfaction[j] >= 0) {
+        addedEachTime += satisfaction[j];
+        total += addedEachTime;
+        j--;
     }
-    if (firstPos === null) {
-        return 0;
-    }
-    for (let i = j; i < satisfaction.length; i++) {
-        total += (satisfaction[i] * multiplier);
-        multiplier++;
-        addedEachTime += satisfaction[i];
-    }
-    // subtract 1 from j to reach first negative
-    j--;
-    while (j >= 0 && (addedEachTime + satisfaction[j]) > 0) {
-        addedEachTime = addedEachTime + satisfaction[j];
+    while (j >= 0 && addedEachTime > Math.abs(satisfaction[j])) {
+        addedEachTime += satisfaction[j];
         total = total + addedEachTime;
         j--;
     }
