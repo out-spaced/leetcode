40c4550ed96a64cabbce40bf974a4446640ba8e9
Alex Tarnawski <69013606+out-spaced@users.noreply.github.com>
2021-10-09 22:53:57 -0500

    Time: 5 ms (41.16%), Space: 8.2 MB (92.75%) - LeetHub

---
+++ generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> parens = { "" };
+    vector<vector<string>> memo = { parens };
+    
+    vector<string> generateParenthesis(int n) { 
+        if (memo.size() > n) {
+            return memo[n];
+        }
+        vector<string> result;
+        if (n == 1) {
+            result.push_back("()");
+        } else {
+            vector<string> prev = generateParenthesis(n - 1);
+            for (int i = 0; i < prev.size(); i++) {
+                string enclose = "(" +  prev[i] + ")";
+                result.push_back(enclose);
+            }
+            for (int i = 1; i < n; i++) {
+                prev = generateParenthesis(i);
+                vector<string> comp = generateParenthesis(n - i - 1);
+                for (int j = 0; j < prev.size(); j++) {
+                    for (int k = 0; k < comp.size(); k++) {
+                        string append = prev[j] + "(" + comp[k] + ")";
+                        result.push_back(append);
+                    }
+
+                }
+            }
+        }
+        memo.push_back(result);
+        return memo[n];
+    }
+};
