23e91365834ad2e46ca7dd9a2ef46929e4767791
Alex Tarnawski <69013606+out-spaced@users.noreply.github.com>
2021-12-08 11:07:51 -0600

    Time: 10 ms (30.08%), Space: 15.4 MB (96.24%) - LeetHub

---
+++ remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.php
@@ -0,0 +1,33 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @param Integer $n
+     * @return ListNode
+     */
+    function removeNthFromEnd($head, $n) {
+        $newHead = new ListNode(0, $head);
+        $firstPtr = $newHead;
+        $secondPtr = $newHead;
+        for ($i = 0; $i <= $n; $i++) {
+            $firstPtr = $firstPtr->next;
+        }
+        while ($firstPtr !== null) {
+            $firstPtr = $firstPtr->next;
+            $secondPtr = $secondPtr->next;
+        }
+        $secondPtr->next = $secondPtr->next->next;
+        return $newHead->next;
+    }
+}
